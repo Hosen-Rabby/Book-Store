e490f9f25738148871955b87ab5b39a7fdb3d97c
Hosen Rabby <mdhosen21018@gmail.com>
2022-12-08 10:42:48 +0600

    updates

--- basket/basket.py
+++ basket/basket.py
@@ -3,5 +3,12 @@ class Basket():
         self.session = request.session
         basket = self.session.get('skey')
         if 'skey' not in request.session:
-            basket = self.session['skey'] = {}
+            basket = self.session['skey'] = {'number':2342}
         self.basket = basket
+
+    def add(self, product):
+        product_id = product.id
+
+        if product_id not in self.basket:
+            self.basket[product_id] = {'price': product.price}
+        self.session.modified = True
--- basket/views.py
+++ basket/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
-
+from django.shortcuts import get_list_or_404
+from django.http import JsonResponse
+from .basket import Basket
+from store.models import Product
 
 
 def basket_summary(request):
     return render(request, 'store/basket/summary.html')
+def basket_add(request):
+    basket = Basket(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('product.id'))
+        product = get_list_or_404(Product, id  = product_id)
+        basket.add(product = product)
+        response = JsonResponse({'test':'data'})
