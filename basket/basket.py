4ebea3d69f8346d67d49e2ec307cbdaeed623f14
Hosen Rabby <mdhosen21018@gmail.com>
2022-12-29 23:53:20 +0600

    updates

--- basket/basket.py
+++ basket/basket.py
@@ -5,22 +5,20 @@ class Basket():
         self.session = request.session
         basket = self.session.get('skey')
         if 'skey' not in request.session:
-            basket = self.session['skey'] = {'number': 20156}
+            basket = self.session['skey'] = {}
         self.basket = basket
 
     def add(self, product, qty):
-
         '''
         adding and updating the users basket session data
         '''
         product_id = product.id
         if product_id not in self.basket:
-            self.basket[product_id] = {'price': str(product.price), 'qty': int(qty)}  
+            self.basket[product_id] = {'price': str(product.price), 'qty': int(qty)}
         self.session.modified = True
 
     def __len__(self):
         '''
         get the basket data and count the quantity of items
         '''
-
-        return sum(item['qty'] for item in self.basket.values()) 
+        return sum(item['qty'] for item in self.basket.values())
