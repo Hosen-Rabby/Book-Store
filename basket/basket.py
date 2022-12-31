dfccb94b63e1850a55cf2d46d993be442e1635c3
Hosen Rabby <mdhosen21018@gmail.com>
2022-12-31 23:35:15 +0600

    delete button

--- basket/basket.py
+++ basket/basket.py
@@ -1,6 +1,8 @@
 from store.models import Product
+from decimal import Decimal
 
 class Basket():
+
     def __init__(self, request):
         self.session = request.session
         basket = self.session.get('skey')
@@ -15,10 +17,44 @@ class Basket():
         product_id = product.id
         if product_id not in self.basket:
             self.basket[product_id] = {'price': str(product.price), 'qty': int(qty)}
-        self.session.modified = True
+
+        self.save()
+
+    def __iter__(self):
+        '''
+        collect the produtc_id in the session data to query the database and return products
+        '''
+        product_ids = self.basket.keys()
+        products = Product.products.filter(id__in=product_ids)
+        basket = self.basket.copy()
+
+
+
+        for product in products:
+            basket[str(product.id)]['product'] = product
+
+        for item in basket.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['qty']
+            yield item
 
     def __len__(self):
         '''
         get the basket data and count the quantity of items
         '''
         return sum(item['qty'] for item in self.basket.values())
+    
+    def get_total_price(self):
+        return sum(Decimal(item['price']) * item['qty'] for item in self.basket.values())
+
+    def delete(self, product):
+        product_id = str(product)
+        print(type(product_id))
+
+        if product_id in self.basket:
+            del self.basket[product_id]
+        self.save()
+
+
+    def save(self):
+        self.session.modified = True
--- basket/views.py
+++ basket/views.py
@@ -6,7 +6,8 @@ from store.models import Product
 
 
 def basket_summary(request):
-    return render(request, 'store/basket/summary.html')
+    basket = Basket(request)
+    return render(request, 'store/basket/summary.html', {'basket' : basket })
 
 def basket_add(request):
     basket = Basket(request)
@@ -15,10 +16,14 @@ def basket_add(request):
         product_qty = int(request.POST.get('productqty'))
         product = get_object_or_404(Product, id=product_id)
         basket.add(product=product, qty=product_qty)
-
         basketqty = basket.__len__()
         response = JsonResponse({'qty': basketqty})
         return response
 
-
- 
+def basket_delete(request):
+    basket =  Basket(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('productid'))
+        basket.delete(product=product_id)
+        response = JsonResponse({'Success': True})
+        return response
