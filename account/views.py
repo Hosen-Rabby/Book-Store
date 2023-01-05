43e39d9e162d8830f83a76aa2ee8f3a8fe835838
Hosen Rabby <mdhosen21018@gmail.com>
2023-01-05 22:18:48 +0600

    updates

--- account/views.py
+++ account/views.py
@@ -3,8 +3,29 @@ from django.shortcuts import render
 from .forms import RegistraionForm 
 
 def account_register(request):
-    if request.user.is_authenticated:
-        return redirect('/')
+    # if request.user.is_authenticated:
+    #     return redirect('/')
     
     if request.method == 'POST':
         registraionForm = RegistraionForm(request.POST)
+        if registraionForm.is_valid():
+            user = registraionForm.save(commit=False)
+            user.email = registraionForm.cleaned_data['email']
+            user.set_password(registraionForm.cleaned_data['password'])
+            user.is_active = False
+            user.save()
+
+            # setup email
+
+            current_site = get_current_site(request)
+            subject = 'Activate your account'
+            message = render_to_string('activate/registration/account_activation_email.html',{
+                'user': user,
+                'domain':current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token':account_activation_token.make_token(user),
+            })
+            user.email_user(subject=sebject, message=message)
+    else:
+        registraionForm = RegistraionForm()
+        return render(request, 'account/registration/register.html', {'form':registraionForm})
