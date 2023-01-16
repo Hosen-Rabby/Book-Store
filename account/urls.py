1010ffeb8f0f540484126e0d5a3d47225115d19e
Hosen Rabby <mdhosen21018@gmail.com>
2023-01-16 21:22:26 +0600

    updates

--- account/urls.py
+++ account/urls.py
@@ -13,15 +13,19 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='account/registration/login.html', form_class=UserLoginForm), name = 'login'),
     # logout
     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name = 'logout'),
-    path('password_reset/', auth_views.PasswordResetView.as_view(
-        template_name='account/user/password_reset_form.html', 
-        success_url = 'password_reset_email_confirm', 
-        email_template_name = 'account/user/password_reset_email.html', form_class=PwdResetForm), name = 'password_reset'),
 
-    path('password_reset_confirm/<slug:uidb64>/<slug:token>', auth_views.PasswordResetConfirmView.as_view(
+    
+    path('password-reset/', auth_views.PasswordResetView.as_view(
+        template_name='account/user/password_reset_form.html', 
+        success_url = 'password-reset-email-confirm', 
+        email_template_name = 'account/user/password_reset_email.html', form_class=PwdResetForm), name = 'password_reset'),
+    path('password-reset-confirm/<slug:uidb64>/<slug:token>', auth_views.PasswordResetConfirmView.as_view(
         template_name='account/user/password_reset_confirm.html',
         success_url = 'password_reset_complete',
-        form_class=PwdResetConfirmForm), name = 'password_reset_confirm'),
+        form_class=PwdResetConfirmForm), name = 'password_reset_confirm'),    
+    path('password-reset/password-reset-email-confirm/', TemplateView.as_view(
+        template_name = 'account/user/reset_status.html'), name = 'password_reset_done'),
+
 
     path('register/', views.account_register, name='register'),
     path('activate/<uidb64>/<token>', views.account_activate, name='activate'),
@@ -30,5 +34,5 @@ urlpatterns = [
     # delete account
     path('frofile/edit/', views.profile_edit, name='profile_edit'),
     path('frofile/delete/', views.profile_delete, name='profile_delete'),
-    path('profile/delete_confirm', TemplateView.as_view(template_name='account/user/delete_confirm.html'), name = 'delete_confirmation'),
+    path('profile/delete-confirm/', TemplateView.as_view(template_name='account/user/delete_confirm.html'), name = 'delete_confirmation'),
 ]
